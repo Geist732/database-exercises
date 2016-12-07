a2e08bd8636df9d4dd779436df077b3be050255d
Nicholas Balderas <geist732@gmail.com>
2016-12-07 16:27:44 -0600

    Updated query utilizing OR clause

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,7 @@
 USE employees;
 DESCRIBE employees;
 
-SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
