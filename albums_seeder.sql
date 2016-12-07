a45f46b6c2aa117f0205dda22bdd26eea132bb1c
Nicholas Balderas <geist732@gmail.com>
2016-12-07 10:32:48 -0600

    completed exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,2 +1,25 @@
 USE codeup_test_db;
-INSERT INTO albums (artist,album,release_date,sales,genre);
+INSERT INTO albums (artist,album,release_date,sales,genre)
+VALUES ('Michael Jackson' , 'Thriller' , 1982 , 46.0, 'ROCK'),
+       ('AC/DC' , 'Back in Black' , 1980 , 26.1, 'ROCK'),
+       ('Pink Floyd' , 'The Dark Side of the Moon' , 1973 ,	24.2 ,'ROCK'),
+       ('Whitney Houston' , 'The Bodyguard' , 1992 , 27.4 , 'R&B'),
+       ('Meat Loaf' , 'Bat Out of Hell' ,	1977 , 20.6 , 'ROCK'),
+       ('Eagles' , 'Their Greatest Hits (1971–1975)' , 1976 , 32.2 , 'ROCK'),
+       ('Bee Gees' , 'Saturday Night Fever' , 1977 , 20.6 ,'DISCO'),
+       ('Fleetwood Mac' , 	'Rumours' ,	1977, 27.9, 'SOFT ROCK'),
+       ('Shania Twain' , 	'Come On Over' , 1997 , 29.6 , 'COUNTRY'),
+       ('Led Zeppelin' , 'Led Zeppelin IV' , 1971 , 29.0, 'ROCK'),
+       ('Adele' , '21' , 2011 , 25.3, 'POP'),
+       ('Michael Jackson' ,	'Bad' ,	1987, 20.3, 'POP'),
+       ('Alanis Morissette'	, 'Jagged Little Pill' , 1995 , 24.8, 'ROCK'),
+       ('Celine Dion' ,	'Falling into You' , 1996, 20.2, 'POP'),
+       ('The Beatles' ,	'Sgt. Pepper''s Lonely Hearts Club Band' , 1967 ,13.1 , 'ROCK' ),
+       ('Eagles' ,	'Hotel California' ,	1976 , 21.5 ,'ROCK'),
+       ('Mariah Carey' , 'Music Box' ,	1993 , 19.0 ,'POP'),
+       ('Michael Jackson' , 'Dangerous' ,	1991 , 17.6, 'POP'),
+
+
+
+
+
