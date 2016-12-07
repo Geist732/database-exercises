ca88c9774432cacdbf8d08530f505e5b01776e4c
Nicholas Balderas <geist732@gmail.com>
2016-12-07 16:30:45 -0600

    Updated query to show employees whos last name starts or ends with the letter 'E'

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,7 +3,7 @@ DESCRIBE employees;
 
 SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
