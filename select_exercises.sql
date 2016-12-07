9b19206ef3bca23ca0025407aef2d14461fa7e4c
Nicholas Balderas <geist732@gmail.com>
2016-12-07 11:38:24 -0600

    completed step 4, searching for the albums released in the 1990's

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,3 +4,9 @@ SELECT album FROM albums WHERE artist = 'Pink Floyd'; /* Step 1 */
 USE codeup_test_db;
 SELECT release_date FROM albums WHERE album = 'Sgt. Pepper''s Lonely Hearts Club Band';  /* STEP 2 */
 
+USE codeup_test_db;
+SELECT genre FROM albums WHERE album = 'nevermind'; /*Step 3*/
+
+USE codeup_test_db;
+SELECT*FROM albums WHERE release_date > 1990 AND release_date <2000;  /* Step 4 */
+
