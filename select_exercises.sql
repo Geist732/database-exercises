a1b8d6f1f241e91a181304bb7e87c0cc65b66856
Nicholas Balderas <geist732@gmail.com>
2016-12-07 11:37:06 -0600

    completed step 2, searching for the release date for a particular album

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,6 @@
 USE codeup_test_db;
-SELECT * FROM albums WHERE artist = 'Pink Floyd'; /* Step 1
+SELECT album FROM albums WHERE artist = 'Pink Floyd'; /* Step 1 */
+
+USE codeup_test_db;
+SELECT release_date FROM albums WHERE album = 'Sgt. Pepper''s Lonely Hearts Club Band';  /* STEP 2 */
+
