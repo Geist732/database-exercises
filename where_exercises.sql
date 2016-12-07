a8c288da3ef759c13c9a79c1bd8b7b4b4099dbe5
Nicholas Balderas <geist732@gmail.com>
2016-12-07 16:33:02 -0600

    Duplicated previous query and update to meet both conditions with AND instead of OR

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,6 +5,8 @@ SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR f
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
