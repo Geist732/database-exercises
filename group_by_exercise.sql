668a54959f4d36d96163812335f65f7b2a80eec3
Nicholas Balderas <geist732@gmail.com>
2016-12-08 09:56:27 -0600

    Updated the query requesting that last_names are sorted to those that contain a 'q' but not 'qu', to also eliminate duplicates in results.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -4,5 +4,9 @@ SELECT DISTINCT title
 FROM titles
 GROUP BY title ASC;
 
-SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
-GROUP BY last_name ASC ;
+SELECT first_name , last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+GROUP BY first_name, last_name;
+
+
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%'
+GROUP BY last_name;
