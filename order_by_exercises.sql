69e7c104dba52409572df6fb9bbd8ac5c566292d
Nicholas Balderas <geist732@gmail.com>
2016-12-08 09:32:42 -0600

    completed the exercise using the ORDER BY clause to complete various tasks

---
+++ order_by_exercises.sql
@@ -0,0 +1,20 @@
+USE employees;
+DESCRIBE employees;
+
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M'
+ORDER BY last_name ,first_name;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E'
+ORDER BY emp_no DESC ;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+/*SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT * FROM employees WHERE birth_date LIKE '%-12-25';*/
+
+SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND (birth_date LIKE '%-12-25')
+ORDER BY hire_date DESC, birth_date DESC ;
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
